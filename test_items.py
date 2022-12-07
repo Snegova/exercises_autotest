4a0297f4d9c215c2c6e6668dccc7b0214b4a885d
Mandarina <99898023+Snegova@users.noreply.github.com>
2022-12-07 20:02:01 +0300

    EX 3.7 - config different languages on interface
    homework for selenium course. Test is looking for "add to basket" button on the page with different languages configurations

---
+++ test_items.py
@@ -0,0 +1,14 @@
+import unittest
+import pytest
+from selenium.webdriver.common.by import By
+import time
+
+def test_guest_should_see_add_to_basket_button(browser, language):
+    link = f"http://selenium1py.pythonanywhere.com/{language}/catalogue/coders-at-work_207/"
+    browser.get(link)
+    time.sleep(5)
+    assert browser.find_elements(By.CSS_SELECTOR, ".btn-primary.btn-add-to-basket"), "Oh no!!! I can't found this button!!!"
+
+if __name__ == "__main__":
+    unittest.main()
+
