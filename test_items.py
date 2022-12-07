1147af5ddb3f0a84d97cd49d385769f0b221a3d9
Mandarina <99898023+Snegova@users.noreply.github.com>
2022-12-07 20:10:26 +0300

    Update test_items.py
    add time sleep

--- test_items.py
+++ test_items.py
@@ -6,7 +6,7 @@ import time
 def test_guest_should_see_add_to_basket_button(browser, language):
     link = f"http://selenium1py.pythonanywhere.com/{language}/catalogue/coders-at-work_207/"
     browser.get(link)
-    time.sleep(5)
+    time.sleep(30) # это вроде было опционально, но я решила облегчить тебе проверку :)
     assert browser.find_elements(By.CSS_SELECTOR, ".btn-primary.btn-add-to-basket"), "Oh no!!! I can't found this button!!!"
 
 if __name__ == "__main__":
